c3ff41c82719b6691f0dbbd210515a80d82eb02f
Alberto Diaz <alberto@diazweb.net>
2022-03-23 16:21:45 +0100

    video 8: vista detallada del producto con show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
 end
