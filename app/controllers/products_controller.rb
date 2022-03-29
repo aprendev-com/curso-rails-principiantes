f0a2f53583ea810efe89cfa34d7e2e01923f2dda
Alberto Diaz <alberto@diazweb.net>
2022-03-29 21:40:14 +0200

    video 13: nuestro primer formulario

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,4 +16,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select '.description', 'PS4 en buen estado'
     assert_select '.price', '150$'
   end
+
+  test 'render a new product form' do
+    get new_product_path
+
+    assert_response :success
+    assert_select 'form'
+  end
 end
