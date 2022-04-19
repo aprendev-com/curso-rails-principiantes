2ef60551a4c0c49a26b6caadd1f1746171041e0b
Alberto Diaz <alberto@diazweb.net>
2022-04-19 20:10:29 +0200

    video 20: subiendo fotos con ActiveStorage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,6 +45,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price)
+    params.require(:product).permit(:title, :description, :price, :photo)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_one_attached :photo
+
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
