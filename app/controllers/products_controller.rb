0dde7777c6cacd9a61aaaa0a08f94debb8a8b388
Alberto Diaz <alberto@diazweb.net>
2022-04-20 10:30:25 +0200

    video 21: nuestro primer refactor

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.with_attached_photo
   end
 
   def show
-    @product = Product.find(params[:id])
+    product
   end
 
   def new
@@ -22,13 +22,11 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    product
   end
 
   def update
-    @product = Product.find(params[:id])
-
-    if @product.update(product_params)
+    if product.update(product_params)
       redirect_to products_path, notice: 'Tu producto se ha actualizado correctamente'
     else
       render :edit, status: :unprocessable_entity
@@ -36,8 +34,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
+    product.destroy
 
     redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente', status: :see_other
   end
@@ -46,5 +43,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :photo)
+  end
+
+  def product
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  delete '/products/:id', to: 'products#destroy'
-  patch '/products/:id', to: 'products#update'
-  post '/products', to: 'products#create'
-  get '/products/new', to: 'products#new', as: :new_product
-  get '/products', to: 'products#index'
-  get '/products/:id', to: 'products#show', as: :product
-  get '/products/:id/edit', to: 'products#edit', as: :edit_product
+  resources :products, path: '/'
 end
