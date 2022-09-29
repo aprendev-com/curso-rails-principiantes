99855dddb00707f180e055f55d6c9dc64ec356ee
Alberto Diaz <alberto@diazweb.net>
2022-09-29 10:37:41 +0200

    video 48: importmaps

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,7 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+import { Alert } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
+
 export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tailwindcss-stimulus-components" # @3.0.4
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.1.0
