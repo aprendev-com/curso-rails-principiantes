490cd09100c413d7c1ee88ffe2567c9dc1ee5521
Alberto Diaz <alberto@diazweb.net>
2022-12-22 17:40:08 +0100

    video 55: refactor con turbo streams

--- app/models/product.rb
+++ app/models/product.rb
@@ -25,4 +25,8 @@ class Product < ApplicationRecord
   def owner?
     user_id == Current.user&.id
   end
+
+  def broadcast
+    broadcast_replace_to self, partial: 'products/product_details', locals: { product: self }
+  end
 end
